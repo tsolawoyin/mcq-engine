92e9674e917a6bfad2558ff955d2e6786f0b276f
tsolawoyin <t.olawoyin@outlook.com>
2026-04-28 10:26:49 +0100

    Update embryo subject code

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -34,7 +34,7 @@ export const topics: Topic[] = [
   {
     id: "introduction-to-embryology-and-cell-division",
     name: "Introduction and Cell division",
-    subject: "embryology",
+    subject: "embryology-101",
     order: 0,
   },
   // Physiology
